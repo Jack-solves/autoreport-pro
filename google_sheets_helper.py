a0f531befb31892b4e4dd9413996058688a9230e
Jack-solves <academy4more@gmail.com>
2025-05-01 23:02:46 +0300

    Update google_sheets_helper.py

--- google_sheets_helper.py
+++ google_sheets_helper.py
@@ -43,7 +43,7 @@ def get_gsheets_service():
                 json.dump(client_secrets, f)
 
             flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
-            auth_url, _ = flow.authorization_url(prompt="consent", redirect_uri="http://localhost")
+            auth_url, _ = flow.authorization_url(prompt="consent")
 
             st.info("🔐 Please authorize access to your Google account:")
             st.markdown(f"[Click here to authorize]({auth_url})")
